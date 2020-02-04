b9e00380f3b4d3715d04975b4deee76e9a8c94d1
tome.naoto.2300@members.co.jp <tome.naoto.2300@members.co.jp>
2020-02-04 18:38:32 +0900

    Implement open menu anim where side area menu.

--- assets/component/js/main.js
+++ assets/component/js/main.js
@@ -3,13 +3,39 @@ window.addEventListener('DOMContentLoaded', function() {
   (function() {
     let allListLabel = document.querySelectorAll('.home-List_Label');
     let allListNamespace = document.querySelectorAll('.home-List_NameSpace');
+    let allListItem = document.querySelectorAll('.home-List_Item');
+    let isOpened;
 
-    allListLabel.forEach(function(listItem) {
-      console.log(listItem);
+    allListLabel.forEach(function(listItem, index) {
+      listItem.addEventListener('click', function(self) {
+        isOpened = self.target.classList.contains('home-List_Label-open');
+
+        if(isOpened) {
+          self.target.classList.remove('home-List_Label-open');
+          self.target.classList.add('home-List_Label-close');
+          allListNamespace[index].classList.add('sw-closed');
+        } else {
+          self.target.classList.add('home-List_Label-open');
+          self.target.classList.remove('home-List_Label-close');
+          allListNamespace[index].classList.remove('sw-closed');
+        }
+      });
     });
 
-    allListNamespace.forEach(function(listItem) {
-      console.log(listItem);
+    allListNamespace.forEach(function(listItem, index) {
+      listItem.addEventListener('click', function(self) {
+        isOpened = self.target.classList.contains('home-List_NameSpace-open');
+
+        if(isOpened) {
+          self.target.classList.remove('home-List_NameSpace-open');
+          self.target.classList.add('home-List_NameSpace-close');
+          allListItem[index].classList.add('sw-closed');
+        } else {
+          self.target.classList.add('home-List_NameSpace-open');
+          self.target.classList.remove('home-List_NameSpace-close');
+          allListItem[index].classList.remove('sw-closed');
+        }
+      });
     });
   })();
 });
