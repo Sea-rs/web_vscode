6d33587ed407620e46bfc2d8ea9a44ac73a651fd
tome.naoto.2300@members.co.jp <tome.naoto.2300@members.co.jp>
2020-02-07 18:20:28 +0900

    fix: convert a class name to a variables.

--- assets/component/js/main.js
+++ assets/component/js/main.js
@@ -4,36 +4,43 @@ window.addEventListener('DOMContentLoaded', function() {
     let allListLabel = document.querySelectorAll('.home-List_Label');
     let allListNamespace = document.querySelectorAll('.home-List_NameSpace');
     let allListItem = document.querySelectorAll('.home-List_Item');
+
+    let homeListLabelOpen = 'home-List_Label-open';
+    let homeListLabelClose = 'home-List_Label-close';
+    let homeListNameSpaceOpen = 'home-List_NameSpace-open';
+    let homeListNameSpaceClose = 'home-List_NameSpace-close';
+    let swClosed = 'sw-closed';
+
     let isOpened;
 
     allListLabel.forEach(function(listItem, index) {
       listItem.addEventListener('click', function(self) {
-        isOpened = self.target.classList.contains('home-List_Label-open');
+        isOpened = self.target.classList.contains(homeListLabelOpen);
 
         if(isOpened) {
-          self.target.classList.remove('home-List_Label-open');
-          self.target.classList.add('home-List_Label-close');
-          allListNamespace[index].classList.add('sw-closed');
+          self.target.classList.remove(homeListLabelOpen);
+          self.target.classList.add(homeListLabelClose);
+          allListNamespace[index].classList.add(swClosed);
         } else {
-          self.target.classList.add('home-List_Label-open');
-          self.target.classList.remove('home-List_Label-close');
-          allListNamespace[index].classList.remove('sw-closed');
+          self.target.classList.add(homeListLabelOpen);
+          self.target.classList.remove(homeListLabelClose);
+          allListNamespace[index].classList.remove(swClosed);
         }
       });
     });
 
     allListNamespace.forEach(function(listItem, index) {
       listItem.addEventListener('click', function(self) {
-        isOpened = self.target.classList.contains('home-List_NameSpace-open');
+        isOpened = self.target.classList.contains(homeListNameSpaceOpen);
 
         if(isOpened) {
-          self.target.classList.remove('home-List_NameSpace-open');
-          self.target.classList.add('home-List_NameSpace-close');
-          allListItem[index].classList.add('sw-closed');
+          self.target.classList.remove(homeListNameSpaceOpen);
+          self.target.classList.add(homeListNameSpaceClose);
+          allListItem[index].classList.add(swClosed);
         } else {
-          self.target.classList.add('home-List_NameSpace-open');
-          self.target.classList.remove('home-List_NameSpace-close');
-          allListItem[index].classList.remove('sw-closed');
+          self.target.classList.add(homeListNameSpaceOpen);
+          self.target.classList.remove(homeListNameSpaceClose);
+          allListItem[index].classList.remove(swClosed);
         }
       });
     });
